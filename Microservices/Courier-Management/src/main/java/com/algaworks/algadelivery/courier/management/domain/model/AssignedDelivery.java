b1c6cc01287d24e37cc78613c4b73acd4dbb242e
seanycarol <seanycarol18@gmail.com>
2025-07-23 23:40:53 -0300

    feat(courier): create courier entities following DDD principles (no ORM mapping yet)

---
+++ Microservices/Courier-Management/src/main/java/com/algaworks/algadelivery/courier/management/domain/model/AssignedDelivery.java
@@ -0,0 +1,24 @@
+package com.algaworks.algadelivery.courier.management.domain.model;
+
+import lombok.*;
+
+import java.time.OffsetDateTime;
+import java.util.UUID;
+
+@NoArgsConstructor(access = AccessLevel.PACKAGE)
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Setter(AccessLevel.PRIVATE)
+@Getter
+public class AssignedDelivery {
+    @EqualsAndHashCode.Include
+    private UUID id;
+
+    private OffsetDateTime assignedAt;
+
+    static AssignedDelivery pending(UUID deliveryId) {
+        AssignedDelivery delivery = new AssignedDelivery();
+        delivery.setId(deliveryId);
+        delivery.setAssignedAt(OffsetDateTime.now());
+        return delivery;
+    }
+}
---
+++ Microservices/Courier-Management/src/main/java/com/algaworks/algadelivery/courier/management/domain/model/Courier.java
@@ -0,0 +1,59 @@
+package com.algaworks.algadelivery.courier.management.domain.model;
+
+import lombok.*;
+
+import java.time.OffsetDateTime;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.UUID;
+
+@NoArgsConstructor(access = AccessLevel.PACKAGE)
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Setter(AccessLevel.PRIVATE)
+@Getter
+public class Courier {
+    @EqualsAndHashCode.Include
+    private UUID id;
+
+    @Setter(AccessLevel.PUBLIC)
+    private String name;
+
+    @Setter(AccessLevel.PUBLIC)
+    private String phone;
+
+    private Integer fulfilledDeliveriesQuantity;
+    private Integer pendingDeliveriesQuantity;
+    private OffsetDateTime lastFulFilledDeliveryAt;
+
+    private List<AssignedDelivery> pendingDeliveries = new ArrayList<>();
+
+    public List<AssignedDelivery> getPendingDeliveries() {
+        return Collections.unmodifiableList(this.pendingDeliveries);
+    }
+
+    public static Courier brandNew(String name, String phone) {
+        Courier courier = new Courier();
+        courier.setId(UUID.randomUUID());
+        courier.setName(name);
+        courier.setPhone(phone);
+        courier.setFulfilledDeliveriesQuantity(0);
+        courier.setPendingDeliveriesQuantity(0);
+        return courier;
+    }
+
+    public void assign(UUID deliveryId) {
+        this.pendingDeliveries.add(AssignedDelivery.pending(deliveryId));
+        this.pendingDeliveriesQuantity++;
+    }
+
+    public void fulfill(UUID deliveryId) {
+        AssignedDelivery delivery = this.pendingDeliveries.stream()
+                .filter(d -> d.getId().equals(deliveryId))
+                .findFirst().orElseThrow();
+        this.pendingDeliveries.remove(delivery);
+        this.pendingDeliveriesQuantity--;
+        this.fulfilledDeliveriesQuantity++;
+        this.setLastFulFilledDeliveryAt(OffsetDateTime.now());
+    }
+}
