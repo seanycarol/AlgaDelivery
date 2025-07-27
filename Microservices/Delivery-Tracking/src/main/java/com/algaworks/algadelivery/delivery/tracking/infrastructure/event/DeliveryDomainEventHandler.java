fc1d80331b0eda2f89cd98718988d461cbb190e0
seanycarol <seanycarol18@gmail.com>
2025-07-27 13:50:26 -0300

    feat(delivery): implement Kafka producer for domain events

--- Microservices/Delivery-Tracking/src/main/java/com/algaworks/algadelivery/delivery/tracking/infrastructure/event/DeliveryDomainEventHandler.java
+++ Microservices/Delivery-Tracking/src/main/java/com/algaworks/algadelivery/delivery/tracking/infrastructure/event/DeliveryDomainEventHandler.java
@@ -8,23 +8,30 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
+import static com.algaworks.algadelivery.delivery.tracking.infrastructure.kafka.KafkaTopicConfig.deliveryEventsTopicName;
+
 @Component
 @Slf4j
 @RequiredArgsConstructor
 public class DeliveryDomainEventHandler {
 
+    private final IntegrationEventPublisher integrationEventPublisher;
+
     @EventListener
     public void handle(DeliveryPlacedEvent event) {
         log.info(event.toString());
+        integrationEventPublisher.publish(event, event.getDeliveryId().toString(), deliveryEventsTopicName);
     }
 
     @EventListener
     public void handle(DeliveryPickUpEvent event) {
         log.info(event.toString());
+        integrationEventPublisher.publish(event, event.getDeliveryId().toString(), deliveryEventsTopicName);
     }
 
     @EventListener
     public void handle(DeliveryFulfilledEvent event) {
         log.info(event.toString());
+        integrationEventPublisher.publish(event, event.getDeliveryId().toString(), deliveryEventsTopicName);
     }
 }
